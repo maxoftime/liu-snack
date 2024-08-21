11e89cdc27c7238bc6eff5d5fc5c211de2e83a71
Max Bergström <max@MacBook-Pro-2.local>
2024-08-21 14:18:26 +0200

    path to file

--- src/functions/writeToDb.js
+++ src/functions/writeToDb.js
@@ -4,8 +4,9 @@ import url from 'url';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 export default (name, content) => {
-    const writeStream = fs.createWriteStream(`${__dirname}/dbs/${name}.txt`, {flags: 'a'});
+    const writeStream = fs.createWriteStream(`${__dirname}/../dbs/${name}.txt`, {flags: 'a'});
     writeStream.write(`${content}\n`);
     writeStream.on('finish', () => { console.log(`Ok`); });
     writeStream.on('error', (err) => { console.error(err) });
